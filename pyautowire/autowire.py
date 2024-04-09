115d15f30dcecf69654629b017a99901478e8007
Tobias Waslowski <tobias.waslowski@thoughtworks.com>
2024-04-09 09:36:34 +0200

    Add comments for clarification

--- pyautowire/autowire.py
+++ pyautowire/autowire.py
@@ -32,23 +32,30 @@ def autowire(*autowire_params):
             ) in autowire_params:  # Check if the parameter is in the pyautowire list
                 if arg_name not in sig.parameters:
                     raise ParameterNotInSignatureError(arg_name)
+                # If the parameter has been provided in kwargs, skip
                 if arg_name in kwargs:
                     continue
                 param = sig.parameters[arg_name]
                 param_type = param.annotation
+                # Check if parameter is of type injectable. This could actually be skipped in the future.
+                # Technically, the only way to write something to the cache with the given abstraction is to use
+                # Injectable.register(), but maybe this mechanism could be changed in the future.
                 if not is_injectable(param_type):
                     raise ParameterNotInjectableError(param_type)
+                # Check if the parameter is in the cache
                 if cache.contains(param_type):
                     logging.debug(
                         "Cache hit for class %s", param_type.get_fully_qualified_name()
                     )
                     kwargs[arg_name] = cache.get(param_type)
                 else:
+                    # Check if an alias for the parameter exists in the cache
                     if cache.contains_alias(arg_name):
                         logging.debug("Cache hit for alias %s", arg_name)
                         kwargs[arg_name] = cache.get_alias(arg_name)
                         continue
                     else:
+                        # Nothing found, raise Error.
                         raise ParameterNotInCacheError(
                             param_type.get_fully_qualified_name()
                         )
